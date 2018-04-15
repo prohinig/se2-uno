3b5e7e4ac07d7b528b864de26f29c077cf27c933
Manuel Hobisch <mahobisch@edu.aau.at>
2018-04-15 18:46:50 +0200

    proof of concept for sensor integration

--- app/src/main/java/games/winchester/unodeluxe/MainActivity.java
+++ app/src/main/java/games/winchester/unodeluxe/MainActivity.java
@@ -1,5 +1,8 @@
 package games.winchester.unodeluxe;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     ImageView deck, stack;
     Deck cardDeck;
     Stack cardStack;
+
+    // The following are used for the shake detection
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,5 +54,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // ShakeDetector initialization
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+            @Override
+            public void onShake(int count) {
+                /*
+                 * The following method, "handleShakeEvent(count):" is a stub //
+                 * method you would use to setup whatever you want done once the
+                 * device has been shook.
+                 */
+                handleShakeEvent(count);
+            }
+        });
+
+    }
+
+    public void handleShakeEvent(int count) {
+        cardDeck.shuffle();
+        Context context = getApplicationContext();
+        CharSequence text = "Karten wurden gemischt.";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
     }
 }
