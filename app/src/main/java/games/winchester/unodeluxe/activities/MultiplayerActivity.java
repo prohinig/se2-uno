7762c3548d0edb5cec2372afd3ea03791c6669c8
David <dlaubrei@gmail.com>
2018-04-17 00:43:08 +0200

    Add Butterknife library and use it

--- app/src/main/java/games/winchester/unodeluxe/activities/MultiplayerActivity.java
+++ app/src/main/java/games/winchester/unodeluxe/activities/MultiplayerActivity.java
@@ -11,23 +11,41 @@ import android.widget.Toast;
 import at.laubi.network.CreationListener;
 import at.laubi.network.MessageSendListener;
 import at.laubi.network.Network;
-import at.laubi.network.NetworkOptions.FallbackCallbacks;
+import at.laubi.network.NetworkCallbacks;
 import at.laubi.network.NetworkOptions;
+import at.laubi.network.exceptions.NetworkException;
 import at.laubi.network.messages.Message;
 import at.laubi.network.session.ClientSession;
 import at.laubi.network.session.HostSession;
 import at.laubi.network.session.Session;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import games.winchester.unodeluxe.R;
 import games.winchester.unodeluxe.messages.SimpleStringMessage;
 import games.winchester.unodeluxe.utils.NetworkUtils;
 
 public class MultiplayerActivity extends AppCompatActivity {
-    TextView ip;
-    Button btnConnect, btnHost, btnSend;
-    EditText etIp, etMessage;
-
     private Network network;
     private Session session;
+
+    @BindView(R.id.tvIp)
+    TextView ip;
+
+    @BindView(R.id.btnConnect)
+    Button btnConnect;
+
+    @BindView(R.id.btnCreateServer)
+    Button btnHost;
+
+    @BindView(R.id.btnSendMessage)
+    Button btnSend;
+
+    @BindView(R.id.inputIp)
+    EditText etIp;
+
+    @BindView(R.id.editText)
+    EditText etMessage;
 
 
     @Override
@@ -35,52 +53,28 @@ public class MultiplayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplayer);
 
-        this.ip = findViewById(R.id.tvIp);
-        this.btnConnect = findViewById(R.id.btnConnect);
-        this.btnHost = findViewById(R.id.btnCreateServer);
-        this.etIp = findViewById(R.id.inputIp);
-        this.etMessage = findViewById(R.id.editText);
-        this.btnSend = findViewById(R.id.btnSendMessage);
-
-        this.btnHost.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onHostClick();
-            }
-        });
-        this.btnConnect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBtnConnect();
-            }
-        });
-        this.btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBtnSend();
-            }
-        });
+        ButterKnife.bind(this);
 
         this.btnSend.setEnabled(false);
 
-        NetworkOptions options = new NetworkOptions();
-        options.callbacks = new FallbackCallbacks() {
+        NetworkCallbacks callbacks = new NetworkCallbacks() {
             @Override
-            public void onException(Exception e, Session s) {
+            public void onExceptionFallback(Exception e, Session s) {
                 toastUiThread(e.getMessage());
                 e.printStackTrace();
             }
 
             @Override
-            public void onMessageReceived(Message message) {
+            public void onReceivedMessage(Message message, Session session) {
                 toastUiThread(message.toString());
             }
         };
 
-        network = new Network(options);
+        network = new Network(null, callbacks);
     }
 
-    private void onHostClick(){
+    @OnClick(R.id.btnCreateServer)
+    void onHostClick(){
         network.createHost(new CreationListener<HostSession>() {
             @Override
             public void onSuccess(HostSession session) {
@@ -88,14 +82,11 @@ public class MultiplayerActivity extends AppCompatActivity {
 
                 that.session = session;
 
-                that.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        that.btnConnect.setEnabled(false);
-                        that.ip.setText( NetworkUtils.getLocalIpAddress());
-                        that.btnSend.setEnabled(true);
-                        that.etIp.setEnabled(false);
-                    }
+                that.runOnUiThread(() -> {
+                    that.btnConnect.setEnabled(false);
+                    that.ip.setText( NetworkUtils.getLocalIpAddress());
+                    that.btnSend.setEnabled(true);
+                    that.etIp.setEnabled(false);
                 });
             }
 
@@ -106,7 +97,8 @@ public class MultiplayerActivity extends AppCompatActivity {
         });
     }
 
-    private void onBtnConnect(){
+    @OnClick(R.id.btnConnect)
+    void onBtnConnect() {
         String text = etIp.getText().toString();
 
         network.createClient(text, new CreationListener<ClientSession>() {
@@ -116,14 +108,11 @@ public class MultiplayerActivity extends AppCompatActivity {
 
                 that.session = session;
 
-                that.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        that.btnConnect.setEnabled(false);
-                        that.btnSend.setEnabled(true);
-                        that.etIp.setEnabled(false);
-                        that.btnHost.setEnabled(false);
-                    }
+                that.runOnUiThread(() -> {
+                    that.btnConnect.setEnabled(false);
+                    that.btnSend.setEnabled(true);
+                    that.etIp.setEnabled(false);
+                    that.btnHost.setEnabled(false);
                 });
             }
 
@@ -134,13 +123,9 @@ public class MultiplayerActivity extends AppCompatActivity {
         });
     }
 
-    private void onBtnSend(){
-        session.send(new SimpleStringMessage(this.etMessage.getText().toString()), new MessageSendListener() {
-            @Override
-            public void onException(Exception e) {
-
-            }
-        });
+    @OnClick(R.id.btnSendMessage)
+    void onBtnSend(){
+        session.send(new SimpleStringMessage(this.etMessage.getText().toString()));
     }
 
     @Override
@@ -151,11 +136,6 @@ public class MultiplayerActivity extends AppCompatActivity {
     }
 
     private void toastUiThread(final String message){
-        this.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(MultiplayerActivity.this, message, Toast.LENGTH_LONG).show();
-            }
-        });
+        this.runOnUiThread(() -> Toast.makeText(MultiplayerActivity.this, message, Toast.LENGTH_LONG).show());
     }
 }
