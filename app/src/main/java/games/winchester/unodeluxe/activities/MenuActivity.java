a5ec3d5ef7ecf173acb2146be6927592d0949d3d
oanitza12 <37271632+oanitza12@users.noreply.github.com>
2018-05-07 13:11:33 +0200

    Update MenuActivity.java

--- app/src/main/java/games/winchester/unodeluxe/activities/MenuActivity.java
+++ app/src/main/java/games/winchester/unodeluxe/activities/MenuActivity.java
@@ -21,8 +21,54 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
         ButterKnife.bind(this);
+        
+         try {
+            this.ok=1;
+            AssetFileDescriptor afd = getAssets().openFd("The Sims 2 - Complete Soundtrack.mp3");
+            player = new MediaPlayer();
+            player.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(),afd.getLength());
+            // Set the looping and play the music.
+            player.setLooping(true);
+            player.prepare();
+            player.setLooping(true);
+            player.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    
+   
+    public void goToTop (View view){
+       Intent intent = new Intent (this, Top.class);
+       startActivity(intent);
+    }
+    public void goToRules (View view){
+      Intent intent = new Intent (this, Rules.class);
+      startActivity(intent);
+    }
+    public void goToMusic (View view){
+       if (ok==1) {
+          player.stop();
+          this.ok=0;
+      }else{
+           try
+           {
+               AssetFileDescriptor afd = getAssets().openFd("The Sims 2 - Complete Soundtrack.mp3");
+           player = new MediaPlayer();
+           player.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(),afd.getLength());
+           // Set the looping and play the music.
+           player.setLooping(true);
+           player.prepare();
+           player.setLooping(true);
+           player.start();
+           this.ok=1;
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+
+   }
+    
     @OnClick(R.id.btnStartGame)
     void onBtnStartGameClick() {
         startGameActivity(null);
