84b892fd0ca85bc2e181742adf67e4dfdf0923b7
oanitza12 <37271632+oanitza12@users.noreply.github.com>
2018-05-07 13:19:13 +0200

    Add files via upload

---
+++ app/src/main/java/games/winchester/unodeluxe/activities/Top.java
@@ -0,0 +1,33 @@
+package unodeluxe.winchester.games.unomenu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.app.Activity;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class Top extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top);
+    }
+    public void goToMain (View view){
+        Intent intent = new Intent (this, MainActivityMenu.class);
+        startActivity(intent);
+        Topclassament();
+
+    }
+    public void Topclassament(){
+
+        ArrayList<String> list =new ArrayList<String>();
+        list.add("Primul");
+        ListView mainListView = (ListView) findViewById( R.id.mainListView );
+    }
+}
