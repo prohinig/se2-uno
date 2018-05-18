e0c9b9c1c8096bba14777bca56fb6efb9397d1c7
David <dlaubrei@gmail.com>
2018-05-18 15:11:57 +0200

    Add documentation for player

--- app/src/main/java/games/winchester/unodeluxe/models/Player.java
+++ app/src/main/java/games/winchester/unodeluxe/models/Player.java
@@ -3,25 +3,49 @@ package games.winchester.unodeluxe.models;
 import java.io.Serializable;
 import java.util.List;
 
-
+/**
+ * Represents a single player
+ */
 public class Player implements Serializable{
 
+    /**
+     * The playes name
+     */
     private String name;
+    /**
+     * Current hand of the player
+     */
     private Hand hand;
 
+    /**
+     * Create a new player
+     * @param name The players name
+     */
     public Player(String name){
         this.name = name;
         this.hand = new Hand();
     }
 
+    /**
+     * Get the players current hand
+     * @return The players current Hand
+     */
     public Hand getHand (){
         return this.hand;
     }
 
+    /**
+     * Add cards to the players hand
+     * @param cards Cards to add to the hand
+     */
     public void addCards(List<Card> cards){
         this.hand.addCards(cards);
     }
 
+    /**
+     * Get the players name
+     * @return The players name
+     */
     public String getName(){
         return this.name;
     }
