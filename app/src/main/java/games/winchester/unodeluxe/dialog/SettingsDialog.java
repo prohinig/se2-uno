ed7b949d2ab47c48627fa49e73dde222729afddd
David <dlaubrei@gmail.com>
2018-06-07 13:12:44 +0200

    Add help to settings dialog

--- app/src/main/java/games/winchester/unodeluxe/dialog/SettingsDialog.java
+++ app/src/main/java/games/winchester/unodeluxe/dialog/SettingsDialog.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Switch;
 
 import games.winchester.unodeluxe.R;
@@ -12,27 +13,36 @@ import games.winchester.unodeluxe.app.Preferences;
 public class SettingsDialog extends AlertDialog.Builder {
 
     Switch switchAdvancedRules;
-
     Switch swichAllowCustomCards;
+
+    LayoutInflater inflater;
 
     private Preferences preferences;
 
     public SettingsDialog(Context context) {
         super(context);
 
-        this.preferences = Preferences.from(context);
+        preferences = Preferences.from(context);
+        inflater = LayoutInflater.from(context);
 
-        View view = LayoutInflater.from(context).inflate(R.layout.layout_options, null);
-        switchAdvancedRules = view.findViewById(R.id.switchStack);
-        swichAllowCustomCards = view.findViewById(R.id.switchCustomCards);
+        View root = createContentView();
 
         loadSettings();
 
         setTitle("Hausregeln konfigurieren");
-        setView(view);
+        setView(root);
         setPositiveButton("Speichern", (d, id) -> saveSettings());
         setNegativeButton("Abbrechen", (d, id) -> {});
-        setNeutralButton("Hilfe", (d, id) -> {});
+    }
+
+    private View createContentView(){
+        View viewRoot = inflater.inflate(R.layout.layout_options, null);
+        switchAdvancedRules = viewRoot.findViewById(R.id.switchStack);
+        swichAllowCustomCards = viewRoot.findViewById(R.id.switchCustomCards);
+        Button btnHelp = viewRoot.findViewById(R.id.btnHelp);
+        btnHelp.setOnClickListener((view) -> displayHelpText());
+
+        return viewRoot;
     }
 
     private void saveSettings() {
@@ -43,5 +53,16 @@ public class SettingsDialog extends AlertDialog.Builder {
     private void loadSettings() {
         switchAdvancedRules.setChecked(preferences.advancedRules());
         swichAllowCustomCards.setChecked(preferences.customCardsAllowed());
+    }
+
+    private void displayHelpText(){
+        View view = inflater.inflate(R.layout.layout_rules_help, null);
+
+        new AlertDialog.Builder(this.getContext())
+                .setTitle(R.string.help_rules)
+                .setView(view)
+                .setPositiveButton("OK", (d, id) -> {})
+                .create()
+                .show();
     }
 }
